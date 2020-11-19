aa263efda49db0d756c8732e9869dd1620c3fa0b
Martin Edvardsen <Martin.Edvardsen@isobar.com>
2020-11-19 11:41:21 +0100

    overlooked other submit event on form

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
+  <form @submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
     <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
     <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
     <div>
@@ -12,7 +12,7 @@
       <label for="bought">bought</label>
       <input v-model="bookData.ownership" type="radio" value="bought">
     </div>
-    <button type="submit" name="button" v-on:submit.prevent="bookSubmit(bookData)">Add Book</button>
+    <button type="submit" name="button">Add Book</button>
   </form>
 </template>
 
